aa28c3be1c90a6abb58074fa2d282e160c651d00
olbrimek <vladimir.olbrimek@seznam.cz>
2024-10-13 08:17:29 +0200

    one

--- answer1.py
+++ answer1.py
@@ -1,13 +1,9 @@
 def check_character(text: str, char: str) -> int:
-    """
-    This function returns the number of occurrences of 'char' in 'text' using a loop.
-    """
-    # Initialize a counter
+
     count = 0
 
-    # Iterate through each character in the string
+
     for c in text:
-        # If the current character matches the target character, increment the counter
         if c == char:
             count += 1
 
@@ -15,4 +11,4 @@ def check_character(text: str, char: str) -> int:
 
 
 
-print(check_character("hello world", "o"))  # Output: 2
+print(check_character("Ahoj jak to jde", "A"))  # Output: 2
